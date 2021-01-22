c6686fc0c113efecea64ad306ab77a21a0abfdbd
SM-Soikot <smsoikot123@gmail.com>
2021-01-22 20:10:11 +0600

    positiive value

--- assignment.js
+++ assignment.js
@@ -1,29 +1,33 @@
+// https://github.com/SM-Soikot/JavaScript
 
         // Unit conversion
 
 function kilometerToMeter(kilometer) {
     return Math.abs(kilometer * 1000);
 }
+
         // Budget calculetor 
 
 function budgetCalculator(watch, phone, laptop) {
-    return (watch * 50) + (phone * 100) + (laptop * 500);
+    return Math.abs((watch * 50) + (phone * 100) + (laptop * 500));
 
 }
+
         // Hotel cost 
 
 function  hotelCost(n) {
 
     if (n <= 10) {
-        return n * 100;
+        return Math.abs(n * 100);
     }
     if (n <= 20) {
-        return 1000 + ((n - 10) * 80);
+        return Math.abs(1000 + ((n - 10) * 80));
     }
     if (n > 20) {
-        return 1800 + ((n - 20) * 50);
+        return Math.abs(1800 + ((n - 20) * 50));
     }
 }
+
         // Mega Friend
 
 function megaFriend(friends) {
